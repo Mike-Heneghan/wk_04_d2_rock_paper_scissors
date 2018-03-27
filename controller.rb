1f0bed13f7e142e317404d5c15aa780cd2180bee
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-27 16:25:38 +0100

    Controller updated to return the string result of rock, paper, scissors. MVP tasks completed althugh code not refactored.

--- controller.rb
+++ controller.rb
@@ -4,6 +4,7 @@ require("pry-byebug")
 
 require_relative("./models/game")
 
-get '/game/:input1/:input2' do
-
+get '/game/:player1/:player2' do
+  game_result = Game.new(params[:player1], params[:player2])
+  return game_result.winner()
 end
